0b9e24e14d6f28effccc852ea73e74ba4517fbab
Mateusz <mat.piech@gmail.com>
2014-11-12 17:37:43 +0100

    [ABRS-17] Add API for loading sources list

--- src/main/java/pl/edu/agh/two/abrs/model/Source.java
+++ src/main/java/pl/edu/agh/two/abrs/model/Source.java
@@ -1,13 +1,6 @@
 package pl.edu.agh.two.abrs.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "source")
