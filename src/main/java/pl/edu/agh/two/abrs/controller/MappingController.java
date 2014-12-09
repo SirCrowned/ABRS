fc6f0d6ea5290922de09cefa3db46df7058ad5a6
molok <olek.lech@gmail.com>
2014-12-09 22:46:04 +0100

    [ABRS-34] refresh view after adding mapping

--- src/main/java/pl/edu/agh/two/abrs/controller/MappingController.java
+++ src/main/java/pl/edu/agh/two/abrs/controller/MappingController.java
@@ -3,10 +3,7 @@ package pl.edu.agh.two.abrs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.agh.two.abrs.model.LocalSchemaColumn;
 import pl.edu.agh.two.abrs.model.global.GlobalSchemaColumn;
 import pl.edu.agh.two.abrs.model.mapping.Mapping;
@@ -35,7 +32,7 @@ public class MappingController {
     private MappingRepository repository;
 
     @RequestMapping(value = "/mapping/add",method = RequestMethod.POST)
-    public String addMapping(@RequestParam("localId") long localId, @RequestParam("globalId") long globalId){
+    public @ResponseBody String addMapping(@RequestParam("localId") long localId, @RequestParam("globalId") long globalId){
         LocalSchemaColumn localSchemaColumn = localSchemaColumnRepository.getOne(localId);
         GlobalSchemaColumn globalSchemaColumn = globalSchemaColumnRepository.getOne(globalId);
         repository.save(new Mapping(globalSchemaColumn, localSchemaColumn));
