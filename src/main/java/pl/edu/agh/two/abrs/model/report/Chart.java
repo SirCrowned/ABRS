f68d5b5fdcec8dc2d355840bcbbc3a630afba225
Wojciech Krzystek <vucalur.work+git@gmail.com>
2015-01-02 20:12:20 +0100

    refactor(model.report): merge ChartData to Chart

--- src/main/java/pl/edu/agh/two/abrs/model/report/Chart.java
+++ src/main/java/pl/edu/agh/two/abrs/model/report/Chart.java
@@ -2,28 +2,62 @@ package pl.edu.agh.two.abrs.model.report;
 
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Element;
+import org.apache.commons.lang3.tuple.Pair;
 import pl.edu.agh.two.abrs.model.report.schema.ChartType;
 import pl.edu.agh.two.abrs.service.export.RendererVisitor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class Chart extends ReportElement {
 
-    private ChartType chartType;
+    private final ChartType chartType;
 
-    private ChartData chartData;
+    private final String name;
 
-    public Chart(ChartType chartType, ChartData chartData) {
+    private Optional<String> xLabel = Optional.empty();
+
+    private Optional<String> yLabel = Optional.empty();
+
+    private List<Pair<Object, Object>> pairs = new ArrayList<>();
+
+    public Chart(ChartType chartType, String name) {
         this.chartType = chartType;
-        this.chartData = chartData;
+        this.name = name;
+    }
+
+    public void setxLabel(String xLabel) {
+        this.xLabel = Optional.of(xLabel);
+    }
+
+    public void setyLabel(String yLabel) {
+        this.yLabel = Optional.of(yLabel);
+    }
+
+    public void addPair(Pair<Object, Object> pair) {
+        pairs.add(pair);
+    }
+
+    public Optional<String> getxLabel() {
+        return xLabel;
+    }
+
+    public Optional<String> getyLabel() {
+        return yLabel;
     }
 
     public ChartType getChartType() {
         return chartType;
     }
 
-    public ChartData getChartData() {
-        return chartData;
+    public List<Pair<Object, Object>> getPairs() {
+        return pairs;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
